2c5a9b270546b96e3966f7f042cadee0c6583fa1
Kevin Chacha <kchachajimenez@byupathway.eud>
2025-09-15 13:48:37 -0500

    Implement grade evaluation logic and user interaction in Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade degree?");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string letter = "";
+
+        if (grade >= 90)
+         {
+            letter = "A";
+            }
+                
+        else if (80 >= grade)
+        {
+            letter = "B";
+            }
+            
+        else if (70 >= grade)
+        {
+            letter = "C";
+            }
+               
+        else if (60 >= grade)
+        {
+            letter = "D";
+            }
+               
+        else  
+        {
+            letter = "F";
+            }
+        int TheLastDigit = grade % 10;
+
+        string sign = "";
+
+        if (TheLastDigit >= 5)
+        {
+            sign = "+";
+
+        } 
+        else if (TheLastDigit < 3)
+        {
+            sign = "-";
+
+        }
+        if (grade >= 93)
+        {
+            sign = ""; 
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        
+
+         Console.WriteLine($"Your grade is {sign}{letter}");
+        
+
+          if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }   
     }
 }
+
